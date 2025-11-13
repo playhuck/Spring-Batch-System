80b8a4fff7cac22d139772decc54ef2465ec3ae7
lay <wngur89@gmail.com>
2025-11-13 11:56:00 +0900

    Initial commit

---
+++ src/test/java/com/system/batch/kill_batch_system/KillBatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.batch.kill_batch_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillBatchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
